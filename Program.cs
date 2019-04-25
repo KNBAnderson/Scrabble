50186b0aad931625afca912f311dec04c1c0a751
Katlin Anderson and Marc Davies <katlin@>
2019-04-25 11:32:38 -0700

    add ui to program

--- Program.cs
+++ Program.cs
@@ -8,7 +8,17 @@ namespace Scrabble.Models
 
     public static void Main()
     {
+      Word newWord = new Word();
+      Console.WriteLine("Please enter a word: ");
+        string userWord = Console.ReadLine();
+        Console.WriteLine("The Scrabble score for  " + userWord  + " is " + newWord.WordScore(userWord) + ".");
+        if (newWord.WordScore(userWord) < 10){
+          Console.WriteLine("That's weak. Do better.");
+        } else {
+          Console.WriteLine("I guess that's ok.");
+        }
 
+        Main();
     }
 
   }
